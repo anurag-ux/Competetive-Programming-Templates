6cf362eb791ed3096bc551b86e1f0700b2bc4b35
Anurag Shandilya <kinganurag8@gmail.com>
2020-07-31 19:29:10 +0530

    basic template for easy questions

---
+++ basic_template.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <bitset>
+#include <limits>
+#include <cmath>
+#include <vector>
+#include <cstdlib>
+#include <string>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+#define MAX 1000000007
+#define fo(i,x) for(int i=0;i<x;i++)
+
+void solve(int n);
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int q;
+    cin >> q;
+	
+    fo(i,q)
+    {
+		//cout<<"Case #"<<i+1<<": ";
+        int n; cin >> n
+        solve(n);
+    }
+}
+
+void solve(int n)
+{
+	
+}
