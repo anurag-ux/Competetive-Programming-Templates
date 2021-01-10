8285767a604b0d04df59e8fbee482634b4712e18
anurag3108 <kinganurag8@gmail.com>
2021-01-10 21:48:38 +0530

    updated template

--- google_kickstart_template.cpp
+++ google_kickstart_template.cpp
@@ -10,12 +10,10 @@ using namespace std;
 #define Graph(n) vector<int> graph[n+1];
 #define addEdgeDirect(adj,u,v){adj[u].push_back(v);}
 #define addEdgeUnDirect(adj,u,v){adj[u].push_back(v); adj[v].push_back(u);}
-#define Prime(n){vector<int> primes; SieveOfEratosthenes(primes); return primes[n];}
 #define Case() ll t; cin >> t; fo(i,t){// cout<<"Case #"<<i+1<<": ";solve();}
 #define io() ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
 void solve();
-void SieveOfEratosthenes(vector<int> &primes);
 
 int main()
 {
@@ -27,21 +25,5 @@ void solve()
 	
 }
 
-void SieveOfEratosthenes(vector<int> &primes)  
-{
-    bool IsPrime[MAX];  
-    memset(IsPrime, true, sizeof(IsPrime));  
-    
-    for (int p = 2; p * p < MAX; p++)  
-    { 
-        if (IsPrime[p] == true){
-            for (int i = p * p; i <  MAX; i += p)  
-                IsPrime[i] = false;  
-        }  
-    }
-    for (int p = 2; p < MAX; p++)  
-       if (IsPrime[p])  
-            primes.push_back(p);             
-}
 
 
